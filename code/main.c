a12d0ec8b45a531c89198392b63c8dfc2ed1337b
Bharat_Manvani <bharatmanvani7@gmail.com>
2023-11-02 18:03:52 +0530

    SystemVerilog DPI Examples and Documents

---
+++ code/main.c
@@ -0,0 +1,9 @@
+  #include "svdpi.h"
+#include <stdio.h>
+
+  void myCFunctio n()
+
+  {
+    printf("HELLO from C function \n");
+    
+  }
---
+++ code/top.sv
@@ -0,0 +1,11 @@
+
+
+module top;
+
+  import "DPI-C" function void myCFunction();
+
+  initial begin
+    myCFunction();
+    $finish;
+  end
+endmodule
